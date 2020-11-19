71f0702c1e74812c4070470208c923297e088a71
Emmanuel40 <52801180+Emmanuel40@users.noreply.github.com>
2020-11-19 06:13:54 +0100

    initial commit

---
+++ BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+
+        public IActionResult List()
+        {
+            var pieListViewModel = new PiesListViewModel();
+
+            pieListViewModel.Pies = _pieRepository.AllPies;
+            pieListViewModel.CurrentCategory = "Cheese Cakes";
+;
+            return View(pieListViewModel);
+        }
+    }
+}
---
+++ BethanysPieShop/Models/PieRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class PieRepository:IPieRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public PieRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Pie> AllPies => _appDbContext.Pies.Include(c => c.Category); 
+        
+
+        public IEnumerable<Pie> PiesOfTheWeek => _appDbContext
+                                                .Pies
+                                                .Include(c => c.Category)
+                                                .Where(p=>p.IsPieOfTheWeek); 
+        
+        public Pie GetPieById(int pieId)=>_appDbContext.Pies.FirstOrDefault(p => p.PieId == pieId);
+        
+    }
+}
